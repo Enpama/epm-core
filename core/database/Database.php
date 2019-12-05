c9cce770a291d70bb4af49f98f9a5ef58cec5e2f
Kadir Yamuk <kadir@enpama.com>
2019-12-05 18:03:07 +0300

    Created basic string and mysql functions

--- core/database/Database.php
+++ core/database/Database.php
@@ -5,19 +5,53 @@ namespace epm\core\database;
 abstract class Database implements DatabaseInterface
 {
 
+    /**
+     *
+     * @var bool $is_free
+     */
     public $is_free = true;
 
+    /**
+     *
+     * @var string $last_query
+     */
     public $last_query;
 
+    /**
+     *
+     * @var string $table_prefix
+     */
     public $table_prefix;
 
+    /**
+     *
+     * @var string $server
+     */
     public $server;
 
+    /**
+     *
+     * @var string $user
+     */
     public $user;
 
+    /**
+     *
+     * @var string $password
+     */
     public $password;
 
+    /**
+     *
+     * @var string $name
+     */
     public $name;
+
+    /**
+     *
+     * @var float $execution_time
+     */
+    public $execution_time = 0.0;
 
     public function __construct(string $server, string $name, string $user, string $password, string $prefix)
     {
--- core/database/MySQL.php
+++ core/database/MySQL.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 namespace epm\core\database;
 
 use PDO;
+use epm\core\lib\StringManipulation;
 
 class MySQL extends Database
 {
@@ -12,6 +13,12 @@ class MySQL extends Database
      * @var \PDO $pdo
      */
     private $pdo;
+
+    /**
+     *
+     * @var \PDOStatement $statement
+     */
+    private $statement;
 
     public function __construct(string $server, string $name, string $user, string $password, string $prefix)
     {
@@ -23,41 +30,88 @@ class MySQL extends Database
     }
 
     public function prepare(string $statement, array $options): Database
-    {}
+    {
+        $this->statement = $this->pdo->prepare($statement, $options);
+        return $this;
+    }
 
     public function prepareLast(): Database
-    {}
+    {
+        return $this->prepare($this->lastQuery);
+    }
 
     public function setFree()
-    {}
+    {
+        $this->is_free = true;
+    }
 
-    public function fetchAll(): array
-    {}
-
-    public function bindParam(string $parameter, string $variable, $type = 'string', $length = null, $options = null): Database
-    {}
-
-    public function execute(bool $debug): bool
-    {}
-
-    public function bindBool(string $parameter, bool $variable): Database
-    {}
-
-    public function bindFloat(string $parameter, float $variable): Database
-    {}
-
-    public function bindDouble(string $parameter, float $variable): Database
-    {}
+    public function setInUse()
+    {
+        $this->is_free = false;
+    }
 
     public function fetch(): array
-    {}
+    {
+        return $this->statement->fetch();
+    }
+
+    public function fetchAll(): array
+    {
+        return $this->statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function bindParam(string $parameter, string $variable, $type = \PDO::PARAM_STR, $length = null,
+        $options = null): Database
+    {
+        if (StringManipulation::startsWith($parameter, ':')) {
+            $parameter = ':' . $parameter;
+        }
+        $this->statement->bindParam($parameter, $variable, $type, $length, $options);
+
+        return $this;
+    }
+
+    public function execute(bool $debug): bool
+    {
+        $start = microtime(true);
+        if ($debug) {
+            // echo '<div>' . $this->statement->debugDumpParams() . '</div>';
+        }
+        $e = $this->statement->execute(null);
+        $this->execution_time = microtime(true) - $start;
+        return $e;
+    }
+
+    public function bindBool(string $parameter, bool $variable): Database
+    {
+        return $this->bindParam($parameter, $variable, PDO::PARAM_BOOL);
+    }
+
+    public function bindFloat(string $parameter, float $variable): Database
+    {
+        return $this->bindDouble($parameter, $variable);
+    }
+
+    public function bindDouble(string $parameter, float $variable): Database
+    {
+        $var = (float) $variable;
+        return $this->bindParam($parameter, $var, PDO::PARAM_STR);
+    }
 
     public function bindString(string $parameter, string $variable): Database
-    {}
+    {
+        $var = $variable !== null ? $variable : '';
+        return $this->bindParam($parameter, $var, PDO::PARAM_STR);
+    }
 
     public function getExecutionTime(): float
-    {}
+    {
+        return $this->execution_time;
+    }
 
     public function bindInt(string $parameter, int $variable): Database
-    {}
+    {
+        $var = (int) $variable;
+        return $this->bindParam($parameter, $var, PDO::PARAM_INT);
+    }
 }
